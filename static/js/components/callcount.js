e9d098a6af6ac2a8935180fb52b374da176a611c
Coleman Nitroy <cnitroy@me.com>
2017-02-23 12:12:08 -0800

    Temporarily turning off the call counter

--- static/js/components/callcount.js
+++ static/js/components/callcount.js
@@ -2,8 +2,12 @@ const html = require('choo/html');
 
 module.exports = (state, prev, send) => {
   return html`
-  <h2 class="callcount" onload=${(e) => send('getTotals')}>
-    Together we've made ${state.totalCalls.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} calls
-  </h2>
+  <div></div>
   `;
+  // temporarily turned this off until we have a more impressive number
+  // return html`
+  // <h2 class="callcount" onload=${(e) => send('getTotals')}>
+  //   Together we've made ${state.totalCalls.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} calls
+  // </h2>
+  // `;
 }
